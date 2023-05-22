a8f366e4edc2f35df99e10548804e10a16faa48d
unknown <rancha.adzany1@gmail.com>
2023-05-22 08:07:34 +0800

    phpmvc

---
+++ app/core/Database.php
@@ -0,0 +1,29 @@
+<?php
+
+class Database {
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $db_name = DB_NAME;
+
+    private $dbh;
+    private $stmt;
+
+    public function __construct()
+    {
+        // data source name
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;
+
+        $option = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
+
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
+
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+}
---
+++ app/models/Mahasiswa_model.php
@@ -0,0 +1,13 @@
+<?php
+
+class Mahasiswa_model {
+    
+
+    
+
+    public function getAllMahasiswa() {
+        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+        $this->stmt->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
